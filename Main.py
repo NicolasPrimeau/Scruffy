cf65b02b05dc819f66c245d838e58ef814c176e0
Nicolas Primeau <nicolas.primeau@gmail.com>
2016-06-12 15:51:16 -0400

    Works great. Some hacky stuff to auto-restart but pretty awesome :)

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import json
 import random
 from pymongo import MongoClient
@@ -6,14 +6,23 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # Up Down Left Right
-ACTIONS = [1, 2, 3, 4]
+ACTIONS = [0, 1, 2, 3]
 GRID_SIZE = 4
 ALPHA = 0.1
 GAMMA = 0.9
+Exploration = 0.05
 
 client = None
 active_count = 0
 game_id = 0
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 
 @app.route("/")
@@ -26,11 +35,11 @@ def api_home():
     return "<ul><li>/api/initialize</li><li>/api/next_action</li><li>/api/reward_update</li></ul>"
 
 
-@app.route("/api/initialize")
+@app.route("/api/initialize", methods=['POST'])
 def initialize():
     global client, active_count, game_id
     if client is None:
-        client = MongoClient('localhost', 27107)
+        client = MongoClient()
         active_count += 1
         random.seed()
         game_id += 1
@@ -39,56 +48,73 @@ def initialize():
 
 @app.route("/api/get_action", methods=['POST'])
 def get_next_action():
-    content = json.load(request.get_json(silent=True))
-    if content is None:
+    if client is None:
+        initialize()
+    state = request.json["state"]
+    illegals = request.json["illegals"]
+    if state is None:
         return json.dumps(random.choice(ACTIONS))
     database = client["AI2048"]
     states = database.states
-    record = states.find_one({"state": content.state})
+    record = states.find_one({"state": state})
     if record is None:
-        record = create_new_entry(content.state, states)
-
-    return json.dumps({"action": get_e_greedy_action(record.actions, content.illegal)}), 201
+        record = create_new_entry(state, states)
+    return json.dumps({"action": get_e_greedy_action(record["actions"], illegals)}), 201
 
 
 @app.route("/api/reward_update", methods=['POST'])
 def update_reward():
-    content = json.load(request.get_json(silent=True))
-    if content is None:
+    state = request.json["state"]
+    next_state = request.json["next_state"]
+    reward = request.json["reward"]
+    action_taken = request.json["action_taken"]
+    if state is None:
         return json.dumps("Reward update is not acceptable"), 501
-    reward_update(content)
+    reward_update(state, float(reward), next_state, action_taken)
     return json.dumps({"game_id": game_id}), 201
 
 
-def reward_update(content):
+def reward_update(state, action_reward, next_state, action_taken):
     database = client["AI2048"]
     states = database.states
-    record = states.find_one({"state": content.state})
+    record = states.find_one({"state": state})
     if record is None:
         return json.dumps("Reward update state doesn't exist????"), 501
 
-    next_record = states.find_one({"state": content.new_state})
+    next_record = states.find_one({"state": next_state})
     if next_record is None:
-        next_record = create_new_entry(content.new_state, states)
+        next_record = create_new_entry(next_state, states)
 
-    reward = record.reward + GAMMA * max(next_record["actions"], key=next_record["actions"].get)
-    reward -= record["actions"][record.action_taken]
-    record["actions"][record.action_taken] += reward
-    states.update({"_id": record["_id"]}, {"$set": record}, upsert=False)
+    reward = action_reward
+    reward += ALPHA*(GAMMA * next_record["actions"][str(max(next_record["actions"], key=next_record["actions"].get))] -
+                    record["actions"][action_taken])
+    record["actions"][action_taken] += reward
+    states.update({'_id': record['_id']}, record)
 
 
 @app.route("/api/restart", methods=['POST'])
 def restart():
-    content = json.load(request.get_json(silent=True))
-    if content is None:
+    state = request.json["state"]
+    next_state = request.json["next_state"]
+    reward = request.json["reward"]
+    action_taken = request.json["action_taken"]
+    if state is None:
         return json.dumps("Reward update is not acceptable"), 501
-    reward_update(content)
+    reward_update(state, float(reward), next_state, action_taken)
     return json.dumps({"game_id": game_id}), 201
 
 
+@app.route("/api/get_script", methods=["GET"])
+def get_script():
+    with open('script.js', 'r') as myfile:
+        data = myfile.read()
+    return Response(data, mimetype='application/javascript')
+
+
 def get_e_greedy_action(actions, illegals):
-    if random.uniform(0, 1) < 0.9:
-        max_value = next(iter(dict.values()))
+    illegals = [str(x) for x in illegals]
+    if random.uniform(0, 1) > Exploration:
+        max_value = next(iter(actions.values()))
         keys = list()
         for key, val in actions.items():
             if val > max_value:
@@ -109,7 +135,7 @@ def create_new_entry(state, collection):
     new_entry["state"] = state
     new_entry["actions"] = dict()
     for i in ACTIONS:
-        new_entry["actions"][i] = random.gauss(0, 1)
+        new_entry["actions"][str(i)] = random.gauss(0, 1)
     collection.insert_one(new_entry)
     return new_entry
 
--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 CUR_STATE = null;
-NEXT_STATE = null;
+CUR_STATE_REWARD = 0;
+ACTION_TAKEN = null;
 
 function Ai() {
 
     this.init = function() {
-        $.ajax({
-            url:'/api/initialize',
-            type:'post',
-            success:function(){}
-    });
+        $.post('http://192.168.0.104:5000/api/initialize', function( data ) {});
     }
 
     this.restart = function() {
         $.ajax({
-            url:'/api/restart',
-            type:'post',
-            success:function(){}
+          type: "POST",
+          contentType: "application/json; charset=utf-8",
+          url: "http://192.168.0.104:5000/api/restart",
+          data: JSON.stringify({"state": CUR_STATE, "next_state": CUR_STATE, "reward": -CUR_STATE_REWARD, "action_taken": ACTION_TAKEN}),
+          success: function( data ) {},
+          dataType: "json"
+        });
+        document.getElementsByClassName("retry-button")[0].click();
     }
 
     this.step = function(grid) {
+        if ($("div.game-message.game-over p").text() != "") {
+            console.log("h")
+            this.restart();
+            return 0;
+        }
         // This method is called on every update.
         // Return one of these integers to move tiles on the grid:
         // 0: up, 1: right, 2: down, 3: left
@@ -36,9 +43,59 @@ function Ai() {
         //              Method returns true if you can move to that direction, false otherwise.
 
         // sample AI:
-        console.log(grid)
+        var state = {};
+        var reward = 0;
+        var cloned = grid.copy();
+        for (var i = 0; i < cloned.cells.length; i++) {
+            for (var j = 0; j < cloned.cells[i].length; j++) {
+                cell = cloned.cells[i][j];
+                cell_name = "" + i + "_" + j
+                state[cell_name] = cell != null ? cell.value : 0
+                reward += cell != null ? cell.value : 0
+            }
+        }
 
-        if (this.foo == null) this.foo = 0;
-        return this.foo++ % 4;
+        if (CUR_STATE != null) {
+            $.ajax({
+              type: "POST",
+              contentType: "application/json; charset=utf-8",
+              url: "http://192.168.0.104:5000/api/reward_update",
+              data: JSON.stringify({"state": CUR_STATE, "next_state": CUR_STATE, "reward": reward, "action_taken": ACTION_TAKEN}),
+              success: function( data ) {},
+              dataType: "json"
+            });
+        }
+
+        CUR_STATE = state;
+        CUR_STATE_REWARD = reward
+
+        var illegals = [];
+        for(var i=0; i<4; i+=1) {
+            cloned = grid.copy();
+            if(!cloned.move(i)) {
+                illegals.push(i);
+            }
+        }
+
+        var action;
+
+        $.ajax({
+              async: false,
+              type: "POST",
+              contentType: "application/json; charset=utf-8",
+              url: "http://192.168.0.104:5000/api/get_action",
+              data: JSON.stringify({"state": CUR_STATE, "illegals": illegals}),
+              success: function( data ) {
+                action = data["action"]
+              },
+              dataType: "json"
+            });
+        ACTION_TAKEN = action
+        return action
     }
+}
+
+function compute_reward(state) {
+
+
 }
