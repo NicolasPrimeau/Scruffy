8cfa5e0f7b519f9b3473a399f1dea175051bd760
Nicolas Primeau <nicolas.primeau@gmail.com>
2016-06-18 10:43:10 -0400

    fix

--- Analytics.py
+++ Analytics.py
@@ -27,7 +27,7 @@ def get_reward_neural_data():
 
 def window_averages(data, window=None):
     if window is None:
-        window = max([int((len(data)/100)), 1])
+        window = max([int((data.count()/100)), 1])
 
     ret_list = list()
     cnt = 0
