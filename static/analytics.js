0844fdc5f7cdebe9172a1b5f53cb7c5ea7ad5488
Nicolas Primeau <nicolas.primeau@gmail.com>
2016-06-18 11:43:55 -0400

    more stats

--- static/analytics.js
+++ static/analytics.js
@@ -1,5 +1,12 @@
 $(function () {
 
+    $.get("/analytics/get_stats", function( data ) {
+        stats = JSON.parse(data);
+        console.log(stats)
+        $("p#game_cnt_val").html(stats["count"])
+        $("p#max_score_val").html(stats["max"])
+    });
+
     $.get( "analytics/get_scores", function( data ) {
 
         var scores_preprocess = JSON.parse(data);
@@ -7,56 +14,73 @@ $(function () {
         for (var i=0; i<scores_preprocess.length; i+=1) {
             scores.push([i, scores_preprocess[i].score]);
         }
-
-        $('#container').highcharts({
-            chart: {
-                type: 'scatter',
-                zoomType: 'xy'
-            },
-            title: {
-                text: 'Scores'
-            },
-            xAxis: {
-                title: {
-                    enabled: true,
-                    text: 'Time'
+        $.get( "analytics/get_fitted_line", function( data ) {
+            var line = JSON.parse(data)
+            line[0][0] = scores[0][0]
+            line[1][0] = scores[scores.length-1][0]
+            $('#container').highcharts({
+                chart: {
+                    type: 'scatter',
+                    zoomType: 'xy'
                 },
-                startOnTick: true,
-                endOnTick: true,
-                showLastLabel: true
-            },
-            yAxis: {
                 title: {
-                    text: 'Score'
-                }
-            },
+                    text: 'Scores'
+                },
+                xAxis: {
+                    title: {
+                        enabled: true,
+                        text: 'Time'
+                    },
+                    startOnTick: true,
+                    endOnTick: true,
+                    showLastLabel: true
+                },
+                yAxis: {
+                    title: {
+                        text: 'Score'
+                    }
+                },
 
-            plotOptions: {
-                scatter: {
-                    marker: {
-                        radius: 5,
+                plotOptions: {
+                    scatter: {
+                        marker: {
+                            radius: 5,
+                            states: {
+                                hover: {
+                                    enabled: true,
+                                    lineColor: 'rgb(100,100,100)'
+                                }
+                            }
+                        },
                         states: {
                             hover: {
-                                enabled: true,
-                                lineColor: 'rgb(100,100,100)'
-                            }
-                        }
-                    },
-                    states: {
-                        hover: {
-                            marker: {
-                                enabled: false
+                                marker: {
+                                    enabled: false
+                                }
                             }
                         }
                     }
-                }
-            },
-            series: [{
-                name: 'Data',
-                color: 'rgba(223, 83, 83, .5)',
-                data: scores
+                },
+                series: [{
+                    name: 'Data',
+                    color: 'rgba(223, 83, 83, .5)',
+                    data: scores
 
-            }]
+                }, {
+                type: 'line',
+                name: 'Regression Line',
+                data: line,
+                marker: {
+                    enabled: false
+                },
+                states: {
+                    hover: {
+                        lineWidth: 0
+                    }
+                },
+                enableMouseTracking: false
+                }]
+            });
         });
     });
 });
